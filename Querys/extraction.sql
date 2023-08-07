f2b4f3f37baab9796611bbfc65f1f0f0caa13ca3
chipigol <guidocasella@hotmail.com>
2023-08-07 17:03:25 -0300

    modify 2nd query

--- Querys/extraction.sql
+++ Querys/extraction.sql
@@ -21,34 +21,20 @@ ORDER BY
 --employees than the mean of employees hired in 2021 for all the departments, ordered
 --by the number of employees hired (descending).
 
-WITH hired_employees_2021 AS (
-    SELECT *
-    FROM hired_employees
-    WHERE EXTRACT(YEAR FROM TO_TIMESTAMP(datetime, 'YYYY-MM-DD"T"HH24:MI:SS"Z"')) = 2021
-),
-employees_per_department AS (
-    SELECT
-        department_id,
-        COUNT(id) AS num_employees_hired_2021
-    FROM hired_employees_2021
-    GROUP BY department_id
-),
-mean_employees_hired AS (
-    SELECT
-        AVG(num_employees_hired_2021) AS mean_employees
-    FROM employees_per_department
+with amount_hired as (
+	select count(id) amount, department_id
+	from hired_employees
+	where EXTRACT(YEAR FROM TO_TIMESTAMP(datetime, 'YYYY-MM-DD"T"HH24:MI:SS"Z"')) = 2021
+	group by department_id
+), 
+
+avg_hired as (
+	select AVG(amount) average
+	from amount_hired
 )
-SELECT
-    d.id AS department_id,
-    d.department AS department_name,
-    COUNT(he.id) AS num_employees_hired
-FROM
-    hired_employees_2021 he
-INNER JOIN departments d ON he.department_id = d.id
-INNER JOIN mean_employees_hired m ON 1=1
-WHERE
-    employees_per_department.num_employees_hired_2021 > m.mean_employees
-GROUP BY
-    d.id, d.department
-ORDER BY
-    num_employees_hired DESC;
+
+select am.department_id id, d.department, am.amount hired
+from amount_hired am 
+inner join avg_hired av on am.amount > av.average
+inner join departments d  on am.department_id = d.id
+order by am.amount desc;
