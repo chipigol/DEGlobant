577468f5db2cefc0270cdea55157ef86576ea9f4
chipigol <guidocasella@hotmail.com>
2023-08-07 12:05:26 -0300

    move createDBFunction

--- app.py
+++ app.py
@@ -10,6 +10,36 @@ app = Flask(__name__)
 DATABASE = 'mydatabase.db'
 
 
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+
+    file = request.files['file']
+
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        return jsonify({'error': 'No file selected'}), 400
+
+    if file and file.filename.endswith('.csv'):
+        # read and process the file contents
+        content = file.stream.read().decode('utf-8')
+        stream = io.StringIO(content)
+        reader = csv.reader(stream)
+        csv_content = list(reader)
+
+        # Print each row to the log
+        for row in csv_content:
+            print(row)
+
+        return jsonify({'message': 'File contents printed to the log'}), 200
+    else:
+        return jsonify({'error': 'Invalid file type, please upload a CSV file'}), 400
+
 def create_database():
     conn = sqlite3.connect(DATABASE)
     cur = conn.cursor()
@@ -46,33 +76,7 @@ def create_database():
     conn.commit()
     conn.close()
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part in the request'}), 400
 
-    file = request.files['file']
-
-    # if user does not select file, browser also
-    # submit an empty part without filename
-    if file.filename == '':
-        return jsonify({'error': 'No file selected'}), 400
-
-    if file and file.filename.endswith('.csv'):
-        # read and process the file contents
-        content = file.stream.read().decode('utf-8')
-        stream = io.StringIO(content)
-        reader = csv.reader(stream)
-        csv_content = list(reader)
-
-        # Print each row to the log
-        for row in csv_content:
-            print(row)
-
-        return jsonify({'message': 'File contents printed to the log'}), 200
-    else:
-        return jsonify({'error': 'Invalid file type, please upload a CSV file'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
