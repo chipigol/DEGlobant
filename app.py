810d7708d59c64eff1e70662368a7b862836eeb1
chipigol <guidocasella@hotmail.com>
2023-08-07 12:21:51 -0300

    testing save_to_db function

--- app.py
+++ app.py
@@ -10,35 +10,35 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 DATABASE = 'mydatabase.db'
 
-
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    # check if the post request has the file part
     if 'file' not in request.files:
         return jsonify({'error': 'No file part in the request'}), 400
 
     file = request.files['file']
 
-    # if user does not select file, browser also
-    # submit an empty part without filename
     if file.filename == '':
         return jsonify({'error': 'No file selected'}), 400
 
+    table_name = request.args.get('table')
+    if not table_name:
+        return jsonify({'error': 'Table name not provided'}), 400
+    
     if file and file.filename.endswith('.csv'):
-        # read and process the file contents
         content = file.stream.read().decode('utf-8')
         stream = io.StringIO(content)
         reader = csv.reader(stream)
         csv_content = list(reader)
 
-        # Print each row to the log
-        for row in csv_content:
-            print(row)
+        save_to_db(table_name, csv_content)
 
-        return jsonify({'message': 'File contents printed to the log'}), 200
+
+
+        return jsonify({'message': f'File contents saved to table {table_name}'}), 200
     else:
         return jsonify({'error': 'Invalid file type, please upload a CSV file'}), 400
-
+    
+    
 def create_database():
     conn = sqlite3.connect(DATABASE)
     cur = conn.cursor()
